f2f1ded4993e5ac05f70e051f47b63552e3643ee
LateNightIceCream <richardgruenert@live.com>
2021-02-20 19:52:16 +0100

    Update Clock.cpp

--- src/displayapp/screens/Clock.cpp
+++ src/displayapp/screens/Clock.cpp
@@ -212,12 +212,12 @@ const char *Clock::DayOfWeekToString(Pinetime::Controllers::DateTime::Days dayOf
 char const *Clock::DaysString[] = {
         "",
         "星期一",
-        "TUESDAY",
-        "WEDNESDAY",
-        "THURSDAY",
-        "FRIDAY",
-        "SATURDAY",
-        "SUNDAY"
+        "星期二",
+        "星期三",
+        "星期四",
+        "星期五",
+        "星期六",
+        "星期日"
 };
 
 char const *Clock::MonthsString[] = {
